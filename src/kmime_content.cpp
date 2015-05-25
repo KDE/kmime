KDE/kmime

commit 5c509e19b676f3efb4d966b68869b23954288a99

KMIME: Do not add additional newlines, while parsing mails.
If testing the signature status of mails, than nothing is allowed to
change. Because the number of newlines, that seperates head and body is
not specified. So make sure that at least two are written.

BUG: 332036
BUG: 332973
FIXED-IN: 4.14.7
REVIEW: 122933

src/kmime_content.cpp
@@ -297,18 +297,24 @@ void Content::clearContents(bool del)
 
 QByteArray Content::encodedContent(bool useCrLf)
 {
-    Q_D(Content);
-    QByteArray e;
+    QByteArray encodedContentData = head();           // return value; initialise with the head data
+    const QByteArray encodedBodyData = encodedBody();
 
-    // Head.
-    e = d->head;
-    e += '\n';
-    e += encodedBody();
+    /* Make sure, that head and body have at least two newlines as seperator, otherwise add one.
+     * If we have enough newlines as sperator, than we should not change the number of newlines
+     * to not break digital signatures
+     */
+    if (!encodedContentData.endsWith("\n\n") &&
+        !encodedBodyData.startsWith("\n\n") &&
+        !(encodedContentData.endsWith("\n") && encodedBodyData.startsWith("\n"))){
+        encodedContentData += '\n';
+    }
+    encodedContentData += encodedBodyData;
 
     if (useCrLf) {
-        return LFtoCRLF(e);
+        return LFtoCRLF(encodedContentData);
     } else {
-        return e;
+        return encodedContentData;
     }
 }
 
@@ -849,20 +855,20 @@ bool Content::decodeText()
     switch (enc->encoding()) {
     case Headers::CEbase64 :
         d->body = KCodecs::base64Decode(d->body);
-        d->body.append("\n");
         break;
     case Headers::CEquPr :
         d->body = KCodecs::quotedPrintableDecode(d->body);
         break;
     case Headers::CEuuenc :
         d->body = KCodecs::uudecode(d->body);
-        d->body.append("\n");
         break;
     case Headers::CEbinary :
         // nothing to decode
-        d->body.append("\n");
     default :
         break;
+    }
+    if (!d->body.endsWith("\n")) {
+        d->body.append("\n");
     }
     enc->setDecoded(true);
     return true;

autotests/contenttest.cpp
@@ -426,7 +426,6 @@ void ContentTest::testMultipartMixed()
         "MIME-Version: 1.0\n"
         "Content-Type: multipart/mixed; boundary=\"simple boundary\"\n"
         "\n"
-        "\n"
         "--simple boundary\n"
         "\n"
         "This is implicitly typed plain US-ASCII text.\n"
@@ -603,7 +602,7 @@ void ContentTest::testParsingUuencoded()
     QVERIFY(!c->contentType()->isText());
     QCOMPARE(c->contentType()->name(), imageName);
     // The uuencoded content as been recoded as base64
-    QCOMPARE(c->encodedContent(), imageBase64);
+    QCOMPARE(c->encodedContent().data(), imageBase64.data());
 
     delete msg;
 }

autotests/data/mails/dontchangemail.mbox
@@ -1,0 +1,49 @@
+From: test@test.de
+To: a@b.de
+Subject: test
+Mime-Version: 1.0
+Content-Type: multipart/signed; micalg=PGP-SHA1;
+ boundary="Sig_/bf9NRhujKrpGsHEluCGG1Ux"; protocol="application/pgp-signature"
+
+--Sig_/bf9NRhujKrpGsHEluCGG1Ux
+Content-Type: multipart/mixed; boundary="MP_/H9EtWvl.za1Pp+XaxTBdQH8"
+
+--MP_/H9EtWvl.za1Pp+XaxTBdQH8
+Content-Type: text/plain; charset=US-ASCII
+Content-Transfer-Encoding: quoted-printable
+Content-Disposition: inline
+
+test
+
+--MP_/H9EtWvl.za1Pp+XaxTBdQH8
+Content-Type: text/plain
+Content-Transfer-Encoding: quoted-printable
+Content-Disposition: attachment; filename=poc.txt
+
+hdsswkrwerifjekdjvsze3diocfgh
+
+--MP_/H9EtWvl.za1Pp+XaxTBdQH8--
+
+--Sig_/bf9NRhujKrpGsHEluCGG1Ux
+Content-Type: application/pgp-signature; name=signature.asc
+Content-Disposition: attachment; filename=signature.asc
+
+-----BEGIN PGP SIGNATURE-----
+Version: GnuPG v2.0.22 (GNU/Linux)
+
+iQIcBAEBAgAGBQJTOSvMAAoJELtUlLP5ziidaY0P/1fhefScUXUoKB52HXx+uRZY
+PAXw0RwIHbOG2V6dcKcC697/XG0xjRItpSkYuK0SzldFVrwarAs8RWqADfqYtL3z
+40LRdUjldAP1PNOLyrBRdJv0TMNwu3waKZ2yug3d48dbmdlt5LU3PUEgY88odClg
+0mP3oWtAp+gzINwJu3B3pW07kdcvyR165w8MnF0hKyXw9mEMrV0WB3pvUbg76Fym
+LsG8haTu+U/qVGSs1NuUEXCPCFxqYo0MPjjjTG3i7yvF/9iBsHTM2kIgbJB5DbGN
+HOy0yOxj1AI8wHNKxG+ylM9jYDAholKXbHJBQhI5LE1pVvdZG7svva1xCl7w0752
+C5bL866CJosH9a3qSJkCnJK20yEvJPFhDsA4HcTln1vo4oh9cNON9mYaheHCLCXP
+N9cMmNMroC2nMW65IkOIkqUncaMRiH58Ykb/EWS9iPqXzyya7T8mCJed5kgb39Iw
+IL1zK6S378nn9qDisGW51Lp4mkiabLMnE62XPoSDwR4zHG8ubd3uGmV1p1RvkETJ
+ciWW31cXtEV80LtnaZzwtKrbqQ/nsvIAv5F6d+iQsh74npFgZJPbS9RS3JjUIHk7
+wnglbXaRq7POR7SssNZAM0x8ylFJ/XCgYKkj2w4WYw437IcNGDdpDXWzZ0CT19oC
+Z/tmc6Ahb8f3J+8Uj+VU
+=/zVi
+-----END PGP SIGNATURE-----
+
+--Sig_/bf9NRhujKrpGsHEluCGG1Ux--

autotests/messagetest.cpp
@@ -323,7 +323,8 @@ void MessageTest::testUtf16()
         "Content-Transfer-Encoding: base64\n"
         "To: =?ISO-8859-1?Q?Fr=C3=A4nz_T=C3=B6ster?= <test@test.de>\n"
         "\n"
-        "//5UAGgAaQBzACAAaQBzACAAVQBUAEYALQAxADYAIABUAGUAeAB0AC4ACgAKAAoACg==\n";
+        "//5UAGgAaQBzACAAaQBzACAAVQBUAEYALQAxADYAIABUAGUAeAB0AC4ACgAKAAo=\n"
+        "\n";
 
     QCOMPARE(msg.encodedContent().data(), newData.data());
 }
@@ -461,7 +462,8 @@ void MessageTest::testBug223509()
     QCOMPARE(msg->subject()->as7BitString().data(), "Subject: Blub");
     QCOMPARE(msg->contents().size(), 0);
     QCOMPARE(msg->contentTransferEncoding()->encoding(), KMime::Headers::CEbinary);
-    QCOMPARE(msg->decodedText().toLatin1().data(), "Bla Bla Bla\n");
+    QCOMPARE(msg->decodedText().toLatin1().data(), "Bla Bla Bla");
+    QCOMPARE(msg->encodedBody().data(), "Bla Bla Bla\n");
 
     // encodedContent() was crashing in this bug because of an invalid assert
     QVERIFY(!msg->encodedContent().isEmpty());
@@ -474,8 +476,7 @@ void MessageTest::testBug223509()
     QCOMPARE(msg2.contents().size(), 0);
     QCOMPARE(msg2.contentTransferEncoding()->encoding(), KMime::Headers::CEbinary);
 
-    QEXPECT_FAIL("", "KMime adds an additional newline", Continue);
-    QCOMPARE(msg2.decodedText().toLatin1().data(), "Bla Bla Bla\n");
+    QCOMPARE(msg2.decodedText().toLatin1().data(), "Bla Bla Bla");
     QCOMPARE(msg2.decodedText(true, true /* remove newlines at end */).toLatin1().data(),
              "Bla Bla Bla");
 }
@@ -596,6 +597,25 @@ void MessageTest::testEncryptedMails()
     QVERIFY(KMime::isInvitation(msg.get()) == false);
     QVERIFY(KMime::isSigned(msg.get()) == false);
 }
+
+void MessageTest::testReturnSameMail()
+{
+    KMime::Message::Ptr msg = readAndParseMail(QLatin1String("dontchangemail.mbox"));
+    QFile file(QLatin1String(TEST_DATA_DIR) + QLatin1String("/mails/dontchangemail.mbox"));
+    const bool ok = file.open(QIODevice::ReadOnly);
+    if (!ok) {
+        qWarning() << file.fileName() << "not found";
+    }
+    Q_ASSERT(ok);
+    QByteArray fileContent = file.readAll();
+    QCOMPARE(msg->encodedContent(), fileContent);
+    QCOMPARE(msg->decodedText(), QLatin1String(""));
+    KMime::Message msg2;
+    msg2.setContent(msg->encodedContent());
+    msg2.parse();
+    QCOMPARE(msg2.encodedContent(), fileContent);
+}
+
 
 KMime::Message::Ptr MessageTest::readAndParseMail(const QString &mailFile) const
 {

autotests/messagetest.h
@@ -45,6 +45,7 @@ private Q_SLOTS:
     void testEncapsulatedMessages();
     void testOutlookAttachmentNaming();
     void testEncryptedMails();
+    void testReturnSameMail();
 
 private:
     KMime::Message::Ptr readAndParseMail(const QString &mailFile) const;

src/kmime_header_parsing.cpp
@@ -2246,7 +2246,10 @@ void extractHeaderAndBody(const QByteArray &content, QByteArray &header, QByteAr
     int pos = content.indexOf("\n\n", 0);
     if (pos > -1) {
         header = content.left(++pos);    //header *must* end with "\n" !!
-        body = content.mid(pos + 1, content.length() - pos - 1);
+        body = content.mid(pos + 1);
+        if (body.startsWith("\n")) {
+            body = "\n" + body;
+        }
     } else {
         header = content;
     }

commit 84b1202c4819a437a609ae08d5c4ce0a7cb33af7

Fix regression introduced in 5c509e19b676f.
Now all KMime unit tests are finally green again.

src/kmime_content.cpp
@@ -663,7 +663,6 @@ void Content::changeEncoding(Headers::contentEncoding e)
         // This is non-textual content.  Re-encode it.
         if (e == Headers::CEbase64) {
             d_ptr->body = KCodecs::base64Encode(decodedContent(), true);
-            d_ptr->body.append("\n");
             enc->setEncoding(e);
             enc->setDecoded(false);
         } else {