3a9651d26a0e169154b58f36df724cde08c9a628
Daniel Vrátil <dvratil@kde.org>
2016-04-23 02:34:10 +0200

    Fix DateFormatterTest
    The test seems to assume LC_TIME=en_US, but the CI uses C locale.
    This ports the test to assume C locale and enforces it via
    set_test_properties() in CMake.

--- autotests/dateformattertest.cpp
+++ autotests/dateformattertest.cpp
@@ -37,10 +37,10 @@ private Q_SLOTS:
 
         auto dt = QDateTime::currentDateTime();
         dt.setTime(QTime(12, 34, 56));
-        QCOMPARE(f.dateString(dt), QString::fromLatin1("Today 12:34 PM"));
+        QCOMPARE(f.dateString(dt), QString::fromLatin1("Today 12:34:56"));
 
         dt.setDate(dt.date().addDays(-1));
-        QCOMPARE(f.dateString(dt), QString::fromLatin1("Yesterday 12:34 PM"));
+        QCOMPARE(f.dateString(dt), QString::fromLatin1("Yesterday 12:34:56"));
 
         dt.setDate(dt.date().addDays(-1));
         QVERIFY(f.dateString(dt).startsWith(QLocale::c().toString(dt, QLatin1String("dddd"))));
