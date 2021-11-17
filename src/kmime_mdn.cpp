c11600fc4c3ce16690471b292ad8b84fe4a512c5
Laurent Montel <montel@kde.org>
2021-11-17 13:43:18 +0100

    GIT_SILENT: use <..> for include

--- src/kmime_mdn.cpp
+++ src/kmime_mdn.cpp
@@ -33,9 +33,9 @@
 #else
 # include <unistd.h>
 #endif
-#include "ki18n_version.h"
+#include <ki18n_version.h>
 #if KI18N_VERSION >= QT_VERSION_CHECK(5, 89, 0)
-#include <klazylocalizedstring.h>
+#include <KLazyLocalizedString>
 #undef I18N_NOOP
 #define I18N_NOOP kli18n
 #endif
