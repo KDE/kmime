29315b50a1c3dc4239b5b50025813c031d0f334f
Volker Krause <vkrause@kde.org>
2024-06-29 11:35:53 +0200

    Add benchmark for parsing plain text messages

---
+++ autotests/messageparserbenchmark.cpp
@@ -0,0 +1,36 @@
+/*
+    SPDX-FileCopyrightText: 2024 Volker Krause <vkrause@kde.org>
+    SPDX-License-Identifier: LGPL-2.0-or-later
+*/
+
+#include <KMime/Message>
+
+#include <QFile>
+#include <QtTest/qtest.h>
+
+using namespace Qt::Literals;
+
+class MessageParserBenchmark : public QObject
+{
+    Q_OBJECT
+private Q_SLOTS:
+    void testPlainTextParse()
+    {
+        QFile file(QLatin1StringView(TEST_DATA_DIR "/mails/plain-text-body.mbox"));
+        QVERIFY(file.open(QIODevice::ReadOnly));
+        const QByteArray data = KMime::CRLFtoLF(file.readAll());
+
+        QBENCHMARK {
+            auto msg = std::make_unique<KMime::Message>();
+            msg->setContent(data);
+            msg->parse();
+            QVERIFY(msg->subject(false));
+            QCOMPARE(msg->subject()->asUnicodeString(), "New Project: Kool Desktop Environment (KDE)"_L1);
+            QVERIFY(msg->decodedText().contains("Kool Desktop Environment"_L1));
+        }
+    }
+};
+
+QTEST_GUILESS_MAIN(MessageParserBenchmark)
+
+#include "messageparserbenchmark.moc"
